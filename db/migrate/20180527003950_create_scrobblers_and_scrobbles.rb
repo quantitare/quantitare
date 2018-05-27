dd2b07c65f8c5900efda3cb867bb1af24dc39b70
Tyler Guillen <tyguillen@gmail.com>
2018-05-26 17:57:39 -0700

    basic schema

---
+++ db/migrate/20180527003950_create_scrobblers_and_scrobbles.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+class CreateScrobblersAndScrobbles < ActiveRecord::Migration[5.2]
+  def change
+    create_table :services do |t|
+      t.string :provider, null: false
+      t.string :name, null: false
+      t.text :token, null: false
+      t.text :secret
+      t.string :uid
+      t.text :refresh_token
+
+      t.references :user, foreign_key: true
+
+      t.datetime :expires_at
+      t.timestamps
+
+      t.index :provider
+      t.index :uid
+    end
+
+    create_table :scrobblers do |t|
+      t.text :options
+      t.string :type, null: false
+      t.string :name
+      t.string :schedule
+      t.string :guid, null: false
+
+      t.boolean :disabled, default: false, null: false
+
+      t.references :user, foreign_key: true
+      t.references :service
+
+      t.datetime :last_scrobbled_at
+      t.timestamps
+
+      t.index :type
+      t.index :schedule
+      t.index :disabled
+    end
+
+    create_table :scrobbles do |t|
+      t.string :type, null: false
+      t.jsonb :data
+
+      t.references :user, foreign_key: true
+      t.references :service
+
+      t.datetime :scrobbled_at
+      t.timestamps
+
+      t.index :type
+      t.index :scrobbled_at
+    end
+  end
+end
---
+++ db/schema.rb
@@ -0,0 +1,87 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2018_05_27_003950) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "scrobblers", force: :cascade do |t|
+    t.text "options"
+    t.string "type", null: false
+    t.string "name"
+    t.string "schedule"
+    t.string "guid", null: false
+    t.boolean "disabled", default: false, null: false
+    t.bigint "user_id"
+    t.bigint "service_id"
+    t.datetime "last_scrobbled_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["disabled"], name: "index_scrobblers_on_disabled"
+    t.index ["schedule"], name: "index_scrobblers_on_schedule"
+    t.index ["service_id"], name: "index_scrobblers_on_service_id"
+    t.index ["type"], name: "index_scrobblers_on_type"
+    t.index ["user_id"], name: "index_scrobblers_on_user_id"
+  end
+
+  create_table "scrobbles", force: :cascade do |t|
+    t.string "type", null: false
+    t.jsonb "data"
+    t.bigint "user_id"
+    t.bigint "service_id"
+    t.datetime "scrobbled_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["scrobbled_at"], name: "index_scrobbles_on_scrobbled_at"
+    t.index ["service_id"], name: "index_scrobbles_on_service_id"
+    t.index ["type"], name: "index_scrobbles_on_type"
+    t.index ["user_id"], name: "index_scrobbles_on_user_id"
+  end
+
+  create_table "services", force: :cascade do |t|
+    t.string "provider", null: false
+    t.string "name", null: false
+    t.text "token", null: false
+    t.text "secret"
+    t.string "uid"
+    t.text "refresh_token"
+    t.bigint "user_id"
+    t.datetime "expires_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["provider"], name: "index_services_on_provider"
+    t.index ["uid"], name: "index_services_on_uid"
+    t.index ["user_id"], name: "index_services_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "scrobblers", "users"
+  add_foreign_key "scrobbles", "users"
+  add_foreign_key "services", "users"
+end

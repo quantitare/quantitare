0fcac362131f578bd728d29577788df485f30da7
Tyler Guillen <tyguillen@gmail.com>
2019-06-12 22:53:39 -0700

    necessary upgrades for webpacker 4

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,9 @@
 const { environment } = require('@rails/webpacker')
 const erb =  require('./loaders/erb')
 const vue =  require('./loaders/vue')
+const { VueLoaderPlugin } = require('vue-loader')
 
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.append('erb', erb)
 environment.loaders.append('vue', vue)
 module.exports = environment

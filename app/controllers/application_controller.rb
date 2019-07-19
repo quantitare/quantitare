4c6c5b4b170d625c1f2b3b88820cf59ba5d1022d
Tyler Guillen <tyguillen@gmail.com>
2019-07-18 20:44:12 -0700

    redirect_to ought to be a public method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   # before_action :configure_permitted_parameters, if: :devise_controller?
 
-  private
-
   def redirect_to(location, *args)
     if request.format.js?
       render js: "window.location='#{location}'"
@@ -16,6 +14,8 @@ class ApplicationController < ActionController::Base
       super
     end
   end
+
+  private
 
   def not_found!
     render file: 'public/404.html', layout: false, status: :not_found
